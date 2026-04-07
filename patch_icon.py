3558a0339760804aa20bb0e13ae530c1e061801e
Florian Zahn (Laptop) <therealkarle@gmail.com>
2026-04-07 14:12:43 +0200

    Made Map layout of combined pdf nicer

---
+++ patch_icon.py
@@ -0,0 +1,8 @@
+﻿from pathlib import Path
+p = Path('polarsteps_pdf_generator.py')
+t = p.read_text('utf-8')
+old = 'color + ";\\">\" +'
+new = 'mutedRouteColor + ";\\">\" +'
+t2 = t.replace(old, new)
+p.write_text(t2, 'utf-8')
+print('changed', t != t2)
